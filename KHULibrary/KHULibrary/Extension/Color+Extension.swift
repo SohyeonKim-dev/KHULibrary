928835f3cc0434be023776ad8e09d12d8ffb66fc
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-06 21:04:16 +0900

    [FEAT] UIColor Extension setting

--- KHULibrary/KHULibrary/Extension/Color+Extension.swift
+++ KHULibrary/KHULibrary/Extension/Color+Extension.swift
@@ -5,4 +5,35 @@
 //  Created by 김소현 on 3/6/24.
 //
 
-import Foundation
+import UIKit.UIColor
+
+extension UIColor {
+    static var KHURed: UIColor {
+        return UIColor(hexCode: "902926")
+    }
+    
+    static var KHUBlue: UIColor {
+        return UIColor(hexCode: "263A74")
+    }
+    
+    static var KHUGold: UIColor {
+        return UIColor(hexCode: "B6985A")
+    }
+}
+
+extension UIColor {
+    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+}
