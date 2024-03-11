54de3c66eb4b9b1062230c436db92530188701a7
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-11 14:18:32 +0900

    [ADD] ReserveViewController, PassViewController added

---
+++ KHULibrary/KHULibrary/ViewController/PassViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PassViewController.swift
+//  KHULibrary
+//
+//  Created by 김소현 on 3/11/24.
+//
+
+import UIKit
+import SnapKit
+
+class PassViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ KHULibrary/KHULibrary/ViewController/ReserveViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ReserveViewController.swift
+//  KHULibrary
+//
+//  Created by 김소현 on 3/11/24.
+//
+
+import UIKit
+import SnapKit
+
+class ReserveViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
